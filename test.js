fd3b37bb09f4946a3c2040ad86f60266ba8f5d67
VASHvic <vforvanish@gmail.com>
2022-04-10 13:17:55 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,183 @@
+{
+  /* <script type="text/javascript">
+    var dom = document.getElementById("container2");
+    var myChart = echarts.init(dom);
+    var app = {};
+
+    var option;
+
+
+
+    option = {
+      series: [
+        {// lmax
+          type: 'gauge',
+          min: 0,
+          max: 115,
+          // axisLine: {
+          //   lineStyle: {
+          //     width: 30,
+          //     color: [
+          //       [0.3, '#67e0e3'],
+          //       [0.7, '#37a2da'],
+          //       [1, '#fd666d']
+          //     ]
+          //   }
+          // },
+          pointer: {
+            itemStyle: {
+              color: '#FD0100'
+            }
+          },
+          // axisTick: {
+          //   distance: -30,
+          //   length: 8,
+          //   lineStyle: {
+          //     color: '#fff',
+          //     width: 2
+          //   }
+          // },
+          splitLine: {
+            distance: -30,
+            length: 30,
+            lineStyle: {
+              color: '#fff',
+              width: 4
+            }
+          },
+          // axisLabel: {
+          //   color: 'auto',
+          //   distance: 40,
+          //   fontSize: 20
+          // },
+          detail: {
+            valueAnimation: true,
+            formatter: '{value} db LMax',
+            fontSize: 30,
+            offsetCenter: [0, '54%'],
+            color: 'crimson'
+          },
+          data: [
+            {
+              value: 62
+            }
+          ]
+        },
+        // leq
+        {
+          type: 'gauge',
+          min: 0,
+          max: 115,
+          // axisLine: {
+          //   lineStyle: {
+          //     width: 30,
+          //     color: [
+          //       [0.3, '#67e0e3'],
+          //       [0.7, '#37a2da'],
+          //       [1, '#fd666d']
+          //     ]
+          //   }
+          // },
+          pointer: {
+            itemStyle: {
+              color: '#FC7D02'
+            }
+          },
+          // axisTick: {
+          //   distance: -30,
+          //   length: 8,
+          //   lineStyle: {
+          //     color: '#fff',
+          //     width: 2
+          //   }
+          // },
+          splitLine: {
+            distance: -30,
+            length: 30,
+            lineStyle: {
+              color: '#fff',
+              width: 4
+            }
+          },
+          // axisLabel: {
+          //   color: 'auto',
+          //   distance: 40,
+          //   fontSize: 20
+          // },
+          detail: {
+            valueAnimation: true,
+            formatter: '{value} db LAeq',
+            fontSize: 26,
+            offsetCenter: [0, '41%'],
+            color: '#FC7D02'
+          },
+          data: [
+            {
+              value: 102
+            }
+          ]
+        },
+        // lmax
+        {
+          type: 'gauge',
+          min: 0,
+          max: 115,
+          splitNumber: 10,
+          axisLine: {
+            lineStyle: {
+              width: 30,
+              color: [
+                [0.48, '#93CE07'],
+                [0.7, '#FC7D02'],
+                [1, '#FD0100']
+              ]
+            }
+          },
+          pointer: {
+            itemStyle: {
+              color: '#93CE07'
+            }
+          },
+          axisTick: {
+            distance: -30,
+            length: 8,
+            lineStyle: {
+              color: '#fff',
+              width: 2
+            }
+          },
+          splitLine: {
+            distance: -30,
+            length: 30,
+            lineStyle: {
+              color: '#fff',
+              width: 4
+            }
+          },
+          axisLabel: {
+            color: 'black',
+            distance: 40,
+            fontSize: 20
+          },
+          detail: {
+            valueAnimation: true,
+            formatter: '{value} db Lmin',
+            fontSize: 25,
+            offsetCenter: [0, '30%'],
+            color: '#93CE07'
+          },
+          data: [
+            {
+              value: 40
+            }
+          ]
+        }
+      ]
+    };
+    if (option && typeof option === 'object') {
+      myChart.setOption(option);
+    }
+
+
+  </script> */
+}
